6840f7ab95dca9fba4fb8d41b96a2d31cbdbd333
abhj <mitraabhijay@gmail.com>
2021-07-22 15:39:58 +0530

    Debug

--- Plane Drawing/Triangle Voxelization/TriangleWithVoxel.cpp
+++ Plane Drawing/Triangle Voxelization/TriangleWithVoxel.cpp
@@ -1,36 +1,36 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-float area(float x_1, float y_1, float x_2, float y_2, float x_3, float y_3) {
+double area(double x_1, double y_1, double x_2, double y_2, double x_3, double y_3) {
 	return abs((x_1 * (y_2 - y_3) + x_2 * (y_3 - y_1) + x_3 * (y_1 - y_2)) / 2.0);
 }
 
-bool isInside(float x_1, float y_1, float x_2, float y_2, float x_3, float y_3, float x, float y) {
+bool isInside(double x_1, double y_1, double x_2, double y_2, double x_3, double y_3, double x, double y) {
 	// A FUNCTION TO CHECK WHETHER POINT P(x, y) LIES INSIDE THE TRIANGLE FORMED BY A(x_1, y_1), B(x_2, y_2) AND C(x_3, y_3)
 
 	// CALCULATE AREA OF TRIANGLE ABC
-	float A = area(x_1, y_1, x_2, y_2, x_3, y_3);
+	double A = area(x_1, y_1, x_2, y_2, x_3, y_3);
 
 	// CALCULATE AREA OF TRIANGLE PBC
-	float A1 = area(x, y, x_2, y_2, x_3, y_3);
+	double A1 = area(x, y, x_2, y_2, x_3, y_3);
 
 	// CALCULATE AREA OF TRIANGLE PAC
-	float A2 = area(x_1, y_1, x, y, x_3, y_3);
+	double A2 = area(x_1, y_1, x, y, x_3, y_3);
 
 	// CALCULATE AREA OF TRIANGLE PAB
-	float A3 = area(x_1, y_1, x_2, y_2, x, y);
+	double A3 = area(x_1, y_1, x_2, y_2, x, y);
 
 	// CHECK IF SUM OF A1, A2 AND A3 IS SAME AS A
 	return A == A1 + A2 + A3;
 }
 
 bool check(int x_1, int y_1, int x_2, int y_2, int x_3, int y_3, int x, int y) {
-	vector<float>X1{(float)x_1 - 0.5, (float)x_1 + 0.5};
-	vector<float>Y1{(float)y_1 - 0.5, (float)y_1 + 0.5};
-	vector<float>X2{(float)x_2 - 0.5, (float)x_2 + 0.5};
-	vector<float>Y2{(float)y_2 - 0.5, (float)y_2 + 0.5};
-	vector<float>X3{(float)x_3 - 0.5, (float)x_3 + 0.5};
-	vector<float>Y3{(float)y_3 - 0.5, (float)y_3 + 0.5};
+	vector<double>X1{(double)x_1 - 0.5, (double)x_1 + 0.5};
+	vector<double>Y1{(double)y_1 - 0.5, (double)y_1 + 0.5};
+	vector<double>X2{(double)x_2 - 0.5, (double)x_2 + 0.5};
+	vector<double>Y2{(double)y_2 - 0.5, (double)y_2 + 0.5};
+	vector<double>X3{(double)x_3 - 0.5, (double)x_3 + 0.5};
+	vector<double>Y3{(double)y_3 - 0.5, (double)y_3 + 0.5};
 
 	// IF THE PIXEL IS WITHIN 0.5 OF ANY BOUNDARY IT IS A PART OF THE PIXELIZATION
 	for (auto x_1 : X1)
@@ -44,22 +44,22 @@ bool check(int x_1, int y_1, int x_2, int y_2, int x_3, int y_3, int x, int y) {
 	return 0;
 }
 
-tuple<float, float, float, float> equationOfPlane(
-    float x_1, float y_1, float z_1,
-    float x_2, float y_2, float z_2,
-    float x_3, float y_3, float z_3) {
+tuple<double, double, double, double> equationOfPlane(
+    double x_1, double y_1, double z_1,
+    double x_2, double y_2, double z_2,
+    double x_3, double y_3, double z_3) {
 
-	float a1 = x_2 - x_1;
-	float b1 = y_2 - y_1;
-	float c1 = z_2 - z_1;
-	float a2 = x_3 - x_1;
-	float b2 = y_3 - y_1;
-	float c2 = z_3 - z_1;
+	double a1 = x_2 - x_1;
+	double b1 = y_2 - y_1;
+	double c1 = z_2 - z_1;
+	double a2 = x_3 - x_1;
+	double b2 = y_3 - y_1;
+	double c2 = z_3 - z_1;
 
-	float a = b1 * c2 - b2 * c1;
-	float b = a2 * c1 - a1 * c2;
-	float c = a1 * b2 - b1 * a2;
-	float d = - a * x_1 - b * y_1 - c * z_1;
+	double a = b1 * c2 - b2 * c1;
+	double b = a2 * c1 - a1 * c2;
+	double c = a1 * b2 - b1 * a2;
+	double d = - a * x_1 - b * y_1 - c * z_1;
 
 	// ax+by+cz+d IS THE EQUATION OF THE PLANE
 	return make_tuple(a, b, c, d);
@@ -113,7 +113,7 @@ void setObj(vector<tuple<int, int, int>>& voxelsCenters) {
 int x_1, y_1, z_1, x_2, y_2, z_2, x_3, y_3, z_3;
 
 
-void xIsUnknown(float a, float b, float c, float d) {
+void xIsUnknown(double a, double b, double c, double d) {
 	// EQUATION OF PLANE OF YZ PLANE IS b j' + c k' + d = 0
 	// THE TRIANGLE PROJECTED ON YZ PLANE HAS VERTICES
 	// (y_1, z_1), (y_2, z_2), (y_3, z_3)
@@ -138,25 +138,24 @@ void xIsUnknown(float a, float b, float c, float d) {
 
 	for (auto &[x, y, z] : voxelsCenters) {
 		// FINDING UPPER AND LOWER LIMITS ON X
-		float upperLimit = ((max(abs(a), abs(b), abs(c)) / 2.0) - (b * (float)y + c * (float)z + d)) / a;
-		float lowerLimit = (- (max(abs(a), abs(b), abs(c)) / 2.0) - (b * (float)y + c * (float)z + d)) / a;
+		double upperLimit = ((max({abs(a), abs(b), abs(c)}) / 2.0) - (b * (double)y + c * (double)z + d)) / a;
+		double lowerLimit = (- (max({abs(a), abs(b), abs(c)}) / 2.0) - (b * (double)y + c * (double)z + d)) / a;
 		assert(upperLimit - lowerLimit <= 1.0);
 		x = (int)upperLimit;
 	}
 
 	// PLOTTING THE VOXELS TO CUBES SURROUNDING THEM
-
 	setObj(voxelsCenters);
 }
 
 
 
 
-void yIsUnknown(float a, float b, float c, float d) {
+void yIsUnknown(double a, double b, double c, double d) {
 
 }
 
-void zIsUnknown(float a, float b, float c, float d) {
+void zIsUnknown(double a, double b, double c, double d) {
 
 }
 
@@ -166,7 +165,7 @@ int main() {
 	// INPUT THE 3D TRIANGLE
 	// A(x_1, y_1, z_1), B(x_2, y_2, z_2), C(x_3, y_3, z_3)
 
-	float a, b, c, d;
+	double a, b, c, d;
 	tie(a, b, c, d) = equationOfPlane(x_1, y_1, z_1, x_2, y_2, z_2, x_3, y_3, z_3);
 
 	// NORMAL VECTOR IS a i' + b j' + c k' + d = 0
